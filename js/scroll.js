21af60c2928ae430ef5185eede52e26d20931cef
junghyun Kim <119426489+wjdgusdbstjd@users.noreply.github.com>
2023-12-10 23:33:05 +0900

    Toast!

---
+++ js/scroll.js
@@ -0,0 +1,261 @@
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-1 .content1-toast-1", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0,
+  y: "30vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-1",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-1 .content1-toast-2", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0.2,
+  y: "30vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-1",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-1 .content1-toast-3", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0.3,
+  y: "30vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-1",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-1 .content1-toast-4", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0.5,
+  y: "30vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-1",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-6 .enjoy", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-6",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-3 h1", 1, {
+  opacity: -1, // 시작값 0
+  delay: 0,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-3",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-8 .t-1", 0.5, {
+  opacity: -1, // 시작값 0
+  delay: 0,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-8",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-8 .o", 0.5, {
+  opacity: -1, // 시작값 0
+  delay: 0.1,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-8",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-8 .a", 0.5, {
+  opacity: -1, // 시작값 0
+  delay: 0.2,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-8",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-8 .s", 0.5, {
+  opacity: -1, // 시작값 0
+  delay: 0.3,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-8",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-8 .t-2", 0.5, {
+  opacity: -1, // 시작값 0
+  delay: 0.4,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-8",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-2 h1", 0.7, {
+  opacity: -1, // 시작값 0
+  delay: 0.4,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-2",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+//-----------------------------------------------------------------
+
+var controller = new ScrollMagic.Controller();
+
+var ani04_1 = gsap.timeline();
+ani04_1.from("#content-2 h2", 0.7, {
+  opacity: -1, // 시작값 0
+  delay: 0.7,
+  y: "50vh", // 오른쪽으로 이동 (값은 원하는 만큼 조절)
+});
+
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#content-2",
+  triggerHook: 0.5,
+})
+  .setTween(ani04_1)
+  .addTo(controller);
+
+
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
+
+//-----------------------------------------------------------------
